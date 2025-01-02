4343be711acd4280418bc835e687542fdf165651
zeynepyilmaz <ylmzbhr1@gmail.com>
2025-01-02 21:47:58 +0300

    form.jsx cerated

---
+++ new-year-class/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import FormEvents from './components/Form';
+
+function App() {
+  return (
+    <div className="App">
+     <FormEvents/>
+    </div>
+  );
+}
+
+export default App;
---
+++ new-year-class/src/components/Form.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+const FormEvents = () => {
+  return (
+    <div>
+      <div className="mt-4 p-3">
+        <div className="text-center">
+          <h1>*********</h1>
+          <h2>Form Events</h2>
+        </div>
+     
+      </div>
+    </div>
+  );
+};
+
+export default FormEvents;

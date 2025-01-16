012c76960b1ec7b979ed31937ba6b917c04d8ada
zeynepyilmaz <ylmzbhr1@gmail.com>
2025-01-16 21:28:35 +0300

    Logos and header images added.

--- 16-Ocak-website-styledComponents/src/components/Header.jsx
+++ 16-Ocak-website-styledComponents/src/components/Header.jsx
@@ -2,7 +2,23 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div>
+      <div>
+        <img src="./images/logo.png" alt="" />
+        <div>
+          <button>Take Courses</button>
+          <button>Talk with Mentor</button>
+        </div>
+      </div>
+      <div>
+      <h1>The IT Career Starts Here</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio sequi expedita eveniet. Qui consequatur sit amet ut quia repudiandae minus!</p>
+      <button>Start Your New Career</button>
+      <button>Details</button>
+    </div>
+    <img src="./images/hero.jpg" alt="" />
+    </div>
+   
   )
 }
 

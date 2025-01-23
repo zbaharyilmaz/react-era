3ddb4438eeb54a27d1eb87c41c61324c9030b3a5
zeynepyilmaz <ylmzbhr1@gmail.com>
2025-01-24 02:39:12 +0300

    Router folder created.

--- 11.router 🧩/src/App.js
+++ 11.router 🧩/src/App.js
@@ -1,12 +1,9 @@
+import React from 'react'
 
-import './App.css';
-
-function App() {
+const App = () => {
   return (
-    <div className="App">
-     
-    </div>
-  );
+    <div>App</div>
+  )
 }
 
-export default App;
+export default App

67a792fd7ffdcc3fdf03c7c36b8225c3daec0ac7
zeynepyilmaz <ylmzbhr1@gmail.com>
2025-01-27 20:37:44 +0300

    MaterialUI  first commit.

---
+++ rc20-MaterialUI/src/components/TypoButtons.jsx
@@ -0,0 +1,10 @@
+
+import React from 'react'
+
+const TypoButtons = () => {
+  return (
+    <div>TypoButtons</div>
+  )
+}
+
+export default TypoButtons
---
+++ rc20-MaterialUI/src/pages/Home.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import TypoButtons from '../components/TypoButtons'
+import CardGrid from '../components/CardGrid'
+import Navbar from '../components/Navbar'
+import TextFieldComp from '../components/TextFieldComp'
+
+const Home = () => {
+  return (
+    <>
+
+    <Navbar/>
+   <TypoButtons/>
+   <TextFieldComp/>
+<CardGrid/>
+
+
+    </>
+  );
+}
+
+export default Home

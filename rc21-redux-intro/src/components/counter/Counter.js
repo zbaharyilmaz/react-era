34fb4b808b35c225094b98f7fdc712df0ebe706d
zeynepyilmaz <ylmzbhr1@gmail.com>
2025-01-29 21:40:58 +0300

    Counter project completed

--- rc21-redux-intro/src/components/counter/Counter.js
+++ rc21-redux-intro/src/components/counter/Counter.js
@@ -1,33 +1,36 @@
+import { useDispatch, useSelector } from "react-redux";
 import "./Counter.css";
+import {arttır, sıfırla,azalt } from '../../redux/actions/counterActions'
 
 const Counter = () => {
 
-
+const{sayac,text1}=useSelector((state)=>state.counterReducer)
+const dispatch1=useDispatch()
 
   return (
     <div className="app">
       <h2 className="counter-header"> Counter with Redux</h2>
-      <h1>{} </h1>
+      <h1>{sayac} </h1>
       {/* sayac */}
-      <h2>{} </h2>
+      <h2>{text1} </h2>
       {/* text1 */}
       <div>
         <button
           className="counter-button positive"
-          //onClick={() => arttır()}
+          onClick={() => dispatch1(arttır())}
         >
           ARTTIR
         </button>
 
         <button
           className="counter-button zero"
-          //onClick={() => sıfırla()}
+          onClick={() =>dispatch1(sıfırla()) }
         >
           RESET
         </button>
         <button
           className="counter-button negative"
-          //onClick={() => azalt()}
+          onClick={() => dispatch1(azalt()) }
           
         >
           AZALT
--- rc21-redux-intro/src/redux/actions/counterActions.jsx
+++ rc21-redux-intro/src/redux/actions/counterActions.jsx
@@ -1,5 +1,5 @@
 
 export const arttır = () => ({type:"INCREASE", payload1:"ARTTIRILDI"})
-export const arttır = () => ({type:"RESET", payload1:"SIIFIRLANDI"})
-export const arttır = () => ({type:"DECREASE", payload1:"AZALTILDI"})
+export const sıfırla= () => ({type:"RESET", payload1:"SIIFIRLANDI"})
+export const azalt= () => ({type:"DECREASE", payload1:"AZALTILDI"})
 

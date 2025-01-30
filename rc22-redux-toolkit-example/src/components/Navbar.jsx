6e230318f0f61a251a6f1cde15c60e5c067df16b
zeynepyilmaz <ylmzbhr1@gmail.com>
2025-01-30 20:48:15 +0300

    Navbar added.

--- rc22-redux-toolkit-example/src/components/Navbar.jsx
+++ rc22-redux-toolkit-example/src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import {  Box} from "@mui/material";
 
+
 const Navbar = () => {
 
 
@@ -11,7 +12,34 @@ const Navbar = () => {
 
   return (
     <Box sx={{ flexGrow: 1 }}>
+      import React from 'react'
+import {  AppBar, Box, Button, Toolbar, Typography} from "@mui/material";
+
+
+const Navbar = () => {
+
+
+
+
+
+
+
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
       
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Updated News
+          </Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+}
+
+export default Navbar
     </Box>
   );
 }
--- rc22-redux-toolkit-example/src/features/yetkiSlice.jsx
+++ rc22-redux-toolkit-example/src/features/yetkiSlice.jsx
@@ -1,1 +1,22 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+
+const yetkiSlice = createSlice({
+
+
+
+    name: 'yetkiSlice',
+    initialState: {
+        email:"",
+        password:"",
+    },
+    reducers: {
+        kullaiciOlustur:(state,{payload})=>{
+           
+        },
+        
+    }
+});
+export const {kullaiciOlustur} = yetkiSlice.actions;
+export default yetkiSlice.reducer;
 

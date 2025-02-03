3768597902b7868998f7fc061dae6d2883231a6e
zeynepyilmaz <ylmzbhr1@gmail.com>
2025-02-03 17:10:07 +0300

    Add submit button to Formik form in Register component

--- rc23-rc28-stockApp/session1-starter/src/pages/Register.jsx
+++ rc23-rc28-stockApp/session1-starter/src/pages/Register.jsx
@@ -5,10 +5,10 @@ import LockIcon from "@mui/icons-material/Lock";
 import image from "../assets/regi.avif";
 import Grid from "@mui/material/Grid";
 import { Link } from "react-router-dom";
-import { Box, TextField } from "@mui/material";
+import { Box, Button, TextField } from "@mui/material";
 import AuthHeader from "../components/AuthHeader";
 import AuthImage from "../components/AuthImage";
-import {Formik} from "formik";
+import { Formik } from "formik";
 
 const Register = () => {
   return (
@@ -45,103 +45,99 @@ const Register = () => {
             Register
           </Typography>
 
-
           <Formik
             initialValues={{
-            userName: "",
-            firstName: "",
-            lastName: "",
-            email: "",
-            password: ""
-
+              userName: "",
+              firstName: "",
+              lastName: "",
+              email: "",
+              password: "",
             }}
             validate={{}}
             onSubmit={{}}
           >
-            {(
-              {
-                values,
-                errors,
-                touched,
-                handleChange,
-                handleBlur,
-                handleSubmit,
-                isSubmitting,
-              }
-            ) => (
+            {({
+              values,
+              errors,
+              touched,
+              handleChange,
+              handleBlur,
+              handleSubmit,
+              isSubmitting,
+            }) => (
               <form action="">
+                <TextField
+                  name="userName"
+                  label="Username"
+                  variant="outlined"
+                  fullWidth
+                  value={values.userName}
+                  onChange={handleChange}
+                  error={touched.userName && errors.userName}
+                  helperText={touched.userName && errors.userName}
+                  onBlur={handleBlur}
+                  margin="normal"
+                />
+                <TextField
+                  name="firstName"
+                  label="First Name"
+                  variant="outlined"
+                  fullWidth
+                  value={values.firstName}
+                  onChange={handleChange}
+                  error={touched.firstName && errors.firstName}
+                  helperText={touched.firstName && errors.firstName}
+                  onBlur={handleBlur}
+                  margin="normal"
+                />
+                <TextField
+                  name="lastName"
+                  label="Last Name"
+                  variant="outlined"
+                  fullWidth
+                  value={values.lastName}
+                  onChange={handleChange}
+                  error={touched.lastName && errors.lastName}
+                  helperText={touched.lastName && errors.lastName}
+                  onBlur={handleBlur}
+                  margin="normal"
+                />
+                <TextField
+                  name="email"
+                  label="Email"
+                  variant="outlined"
+                  fullWidth
+                  value={values.email}
+                  onChange={handleChange}
+                  error={touched.email && errors.email}
+                  helperText={touched.email && errors.email}
+                  onBlur={handleBlur}
+                  margin="normal"
+                  type="email"
+                />
+                <TextField
+                  name="password"
+                  label="Password"
+                  variant="outlined"
+                  fullWidth
+                  value={values.password}
+                  onChange={handleChange}
+                  error={touched.password && errors.password}
+                  helperText={touched.password && errors.password}
+                  onBlur={handleBlur}
+                  margin="normal"
+                  type="password"
+                />
 
-                <TextField
-                name="userName"
-                label="Username" 
-                variant="outlined" 
+                <Button 
+                variant="contained"
                 fullWidth
-                value={values.userName}
-                onChange={handleChange}
-                error={touched.userName&& errors.userName}
-                helperText={touched.userName&&errors.userName}
-                onBlur={handleBlur}
-                margin="normal"
-                />
-                <TextField
-                name="firstName"
-                label="First Name" 
-                variant="outlined" 
-                fullWidth
-                value={values.firstName}
-                onChange={handleChange}
-                error={touched.firstName&& errors.firstName}
-                helperText={touched.firstName&&errors.firstName}
-                onBlur={handleBlur}
-                margin="normal"
-                />
-                <TextField
-                name="lastName"
-                label="Last Name" 
-                variant="outlined" 
-                fullWidth
-                value={values.lastName}
-                onChange={handleChange}
-                error={touched.lastName&& errors.lastName}
-                helperText={touched.lastName&&errors.lastName}
-                onBlur={handleBlur}
-                margin="normal"
-                />
-                <TextField
-                name="email"
-                label="Email" 
-                variant="outlined" 
-                fullWidth
-                value={values.email}
-                onChange={handleChange}
-                error={touched.email&& errors.email}
-                helperText={touched.email&&errors.email}
-                onBlur={handleBlur}
-                margin="normal"
-                type="email"
-                />
-                <TextField
-                name="password"
-                label="Password" 
-                variant="outlined" 
-                fullWidth
-                value={values.password}
-                onChange={handleChange}
-                error={touched.password&& errors.password}
-                helperText={touched.password&&errors.password}
-                onBlur={handleBlur}
-                margin="normal"
-                type="password"
-                />
+                type="submit"
+                >SUBMIT
+                </Button>
               </form>
-
             )}
           </Formik>
-
-
-
-
-
 
           <Box sx={{ textAlign: "center", mt: 2, color: "secondary.main" }}>
             <Link to="/">Already have an account? Sign in</Link>

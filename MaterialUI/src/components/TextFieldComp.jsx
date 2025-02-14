72953a58f4a2e0c71b91102fff96153a88e04598
zeynepyilmaz <ylmzbhr1@gmail.com>
2025-02-14 23:23:42 +0300

    Enhance TextField component with additional currency options and error handling

--- MaterialUI/src/components/TextFieldComp.jsx
+++ MaterialUI/src/components/TextFieldComp.jsx
@@ -2,17 +2,32 @@ import { Box, Container, TextField, Typography } from "@mui/material";
 import { useState } from "react";
 
 const TextFieldComp = () => {
-   const [error, setError] = useState(false);
-
+  const [error, setError] = useState(false);
+  const currencies = [
+    {
+      value: 'USD',
+      label: '$',
+    },
+    {
+      value: 'EUR',
+      label: '€',
+    },
+    {
+      value: 'BTC',
+      label: '฿',
+    },
+    {
+      value: 'JPY',
+      label: '¥',
+    },
+  ];
   return (
     <div>
       <Container>
         <Typography variant="h4" align="center" gutterBottom>
           Costumes
         </Typography>
-        <Box 
-        onClick={()=>setError(true)}
-        sx={{ m: 2 }}>
+        <Box onClick={() => setError(true)} sx={{ m: 2 }}>
           <TextField
             required
             id="outlined-basic"
@@ -20,15 +35,45 @@ const TextFieldComp = () => {
             variant="outlined"
             defaultValue="Enter an email"
             margin="dense"
+            sx={{ m: 2 }}
           />
 
           <TextField
             id="outlined-helperText"
             label="Country"
             defaultValue="  Enter a country"
-            helperText={error? "Please enter a valid country": ""}
+            helperText={error ? "Please enter a valid country" : ""}
             variant="outlined"
-            
+            sx={{ m: 2 }}
+          />
+          <TextField
+            color="secondary"
+            id="outlined-secondary"
+            label="Secondary"
+            error={error}
+            label="Error"
+            defaultValue="Hello World"
+            variant="outlined"
+            sx={{ m: 2 }}
+            fullWidth
+          />
+          <TextField label="Outlined secondary" color="secondary" focused />
+            <TextField
+                label="Filled primary"
+                variant="filled"
+                color="primary"
+                focused/>
+          <TextField
+            label="Filled"
+            variant="filled"
+            color="success"
+            focused
+          />
+          <TextField
+            label="Standard warning"
+            variant="standard"
+            color="warning"
+            focused
           />
         </Box>
       </Container>

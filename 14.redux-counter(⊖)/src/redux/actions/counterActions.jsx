5efb4cf9b22be29b485649274b7d64fa16b75e09
zeynepyilmaz <ylmzbhr1@gmail.com>
2025-02-27 23:56:59 +0300

    Remove unused Redux files and assets from the project

--- 14.redux-counter(⊖)/src/redux/actions/counterActions.jsx
+++
@@ -1,5 +0,0 @@
-//garson
-
-export const arttir=()=>({type:"INCREASE", payload1:"ARTTIRILDI"})
-export const sifirla=()=>({type:"RESET"})
-export const azalt=()=>({type:"DECREASE", payload1:"AZALTILDI"})
--- 14.redux-counter(⊖)/src/redux/store.jsx
+++
@@ -1,8 +0,0 @@
-import {legacy_createStore as createStore } from "redux";
-import counterReducer from "./reducers/counterReducer";
-
-
-
-
-export const store = createStore(counterReducer);
-
